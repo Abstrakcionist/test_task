f0332f829669a0aa0c3e21004e628c2da92cfa00
Abstrakcionist <kondratev04g@mail.ru>
2023-02-01 21:29:54 +0500

    добавил свайпер на недавнее и хиты

--- swiper.js
+++ swiper.js
@@ -1,8 +1,19 @@
-const swiper = new Swiper('.swiper', {
+const swiper1 = new Swiper('.swiper1', {
     speed: 1000,
     spaceBetween: 10,
     loop: true,
     autoplay:{
         delay: 5000,
+    },
+});
+
+
+const swiper2 = new Swiper('.swiper2', {
+    speed: 1000,
+    spaceBetween: 10,
+    loop: true,
+    navigation:{
+        nextEl: '.swiper2-button-next',
+        prevEl: '.swiper2-button-prev',
     },
 });
